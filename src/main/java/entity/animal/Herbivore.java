951c100f638316f8df12ae78ec7656a9e3d60985
Vladislavkrivenko <vladislavkrivenko7@gmail.com>
2025-05-06 19:30:57 +0300

    feat: добавления класов животных  в animal

--- src/main/java/entity/animal/Herbivore.java
+++ src/main/java/entity/animal/Herbivore.java
@@ -1,5 +1,24 @@
 package entity.animal;
 
-public class Herbivore {
+import coordinates.Coordinates;
+import entity.Grass;
+import mapManager.EntityManager;
+import mapManager.MapService;
+
+public class Herbivore extends Creature {
+    public Herbivore(Coordinates coordinates, String typeOfAnimal, int animalSpeed, EntityManager entityManager, MapService mapService, SearchAlgorithm searchAlgorithm) {
+        super(coordinates, "Rabbit", 2, Grass.class, entityManager, mapService,searchAlgorithm);
+    }
+
+    @Override
+    public void setPosition(Coordinates newPosition) {
+        super.setPosition(newPosition);
+    }
+
+    @Override
+    public String getSprite() {
+        return "🐰";
+    }
 
 }
+
--- src/main/java/entity/animal/Predator.java
+++ src/main/java/entity/animal/Predator.java
@@ -1,5 +1,25 @@
 package entity.animal;
 
+import coordinates.Coordinates;
+import mapManager.EntityManager;
+import mapManager.MapService;
+
 public class Predator  {
+    public class Predator extends Creature {
+        public Predator(Coordinates coordinates, String typeOfAnimal, int animalSpeed, EntityManager entityManager, MapService mapService, SearchAlgorithm searchAlgorithm) {
+            super(coordinates, "Wolf", 2, Herbivore.class, entityManager, mapService,searchAlgorithm);
+
+        }
+
+        @Override
+        public void setPosition(Coordinates newPosition) {
+            super.setPosition(newPosition);
+        }
+
+        @Override
+        public String getSprite() {
+            return "🐺";
+        }
+    }
 
 }
